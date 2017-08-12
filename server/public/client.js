46bc407345765f95c30da419a0d6da1b48a6911a
Tom Ritland <t_ritland@yahoo.com>
2017-08-12 15:35:24 -0500

    displayed tasks on DOM and added delete buttons and check boxes

--- server/public/client.js
+++ server/public/client.js
@@ -1,34 +1,46 @@
-$(document).ready(function(){
-    $('#addTaskButton').on('click', function (){
-    newTask = $('#taskInput').val();
-    var objectToSend = {
-      task: newTask,
-    };
-    saveTask(objectToSend); //calls saveKoala function with new task object
+$(document).ready(function () {
+    getTasks();
+
+    $('#addTaskButton').on('click', function () {
+        newTask = $('#taskInput').val();
+        var objectToSend = {
+            task: newTask,
+        };
+        saveTask(objectToSend);
     });
 
 });
 
-function saveTask(newTask){
+function saveTask(newTask) {
     $.ajax({
         url: '/tasks',
         method: 'POST',
         data: newTask,
-        success: function (response){
+        success: function (response) {
             console.log(response);
             getTasks(); //gets updated task table
             $('#taskInput').val(''); //clears input field
-        }        
+        }
     });
 };
 
-function getTasks(){
- $.ajax({
-    url: '/tasks',
-    method: 'GET',
-    success: function (response) {
-      console.log('got these tasks: ', response);
-      //showTasks(response);
-    } 
-  });
+function getTasks() {
+    $.ajax({
+        url: '/tasks',
+        method: 'GET',
+        success: function (response) {
+            showTasks(response);
+        }
+    });
+};
+
+function showTasks(tasks) {
+    $('#viewTasks').empty();
+    console.log(tasks);
+    for (var i = 0; i < tasks.length; i++) {
+        var task = tasks[i];
+        var $taskRow = $('<tr><td>' + task.task_list + '</td><td><input class="checkBox" type="checkbox"></td><td><button class="deleteButton"> Delete </button></td></tr>');
+
+        $('#viewTasks').append($taskRow);
+    }
 };
